ef900605fbf2f803ec25379974fba23cff0a0bde
Prashant Jha <jha.prashant654@gmail.com>
2018-06-05 11:49:28 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {GithubService} from './services/github.services';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+  <nav class="navbar navbar-default" >
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Coditas Assignment</a>
+        </div>
+        </div>
+    </nav>
+  <div class = "container">
+  <profile> </profile>
+  </div>`
+  ,
+  providers : [GithubService]
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent  { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import {ProfileComponent} from './components/profile.component'
+import { AppComponent }  from './app.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, HttpModule,FormsModule ],
+  declarations: [ AppComponent, ProfileComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
